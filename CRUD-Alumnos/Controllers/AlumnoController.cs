3e84f58c527e9d98e0de44b6a9f46933d546cd37
Omar Alvarado <omar.alvarado@grupodiusframi.com.co>
2020-03-20 16:31:31 -0500

    first commit

---
+++ CRUD-Alumnos/Controllers/AlumnoController.cs
@@ -0,0 +1,195 @@
+﻿using CRUD_Alumnos.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_Alumnos.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            try
+            {
+                /*int edad = 18;
+                string sql = @"select 
+                a.Id as Id,
+                a.Nombre as Nombre,
+                a.Apellidos as Apellidos,
+                a.Edad as Edad,
+                a.Sexo as Sexo,
+                a.FechaRegistro as FechaRegistro,
+                c.Nombre as NombreCiudad
+                 from Alumno a
+                inner join Ciudad c on c.Id=a.Id
+                where a.Edad > @edad";*/
+
+                using (var db = new AlumnosContext())
+                {
+                   // return View(db.Database.SqlQuery<AlumnoCE>(sql, 
+                     //   new SqlParameter("@edad",edad)).ToList());
+                    var data = from a in db.Alumno
+                               join c in db.Ciudad on a.CodCiudad equals c.Id
+                               select new AlumnoCE()
+                               {
+                                   Id = a.Id,
+                                   Nombre = a.Nombre,
+                                   Apellidos = a.Apellidos,
+                                   Edad = a.Edad,
+                                   Sexo = a.Sexo,
+                                   NombreCiudad = c.Nombre,
+                                   FechaRegistro = a.FechaRegistro
+                               };
+                    return View(data.ToList());
+                    //where a.Equals > 18
+                    //select a
+                    //List<Alumno> lista = db.Alumno.Where(a => a.Edad > 18).ToList();
+                    //
+                    
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            // AlumnosContext db = new AlumnosContext();
+            //List<Alumno> lista = db.Alumno.Where(a => a.Edad > 18).ToList();
+
+        }
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Agregar(Alumno a)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                try
+                {
+                    using (var db = new AlumnosContext())
+                    {
+                        a.FechaRegistro = DateTime.Now;
+                        db.Alumno.Add(a);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    ModelState.AddModelError("", "Error al agregar el Alumno - " + ex.Message);
+                }
+            }
+
+            return View();
+        }
+        public ActionResult Editar(int id)
+        {
+            try
+            {
+                using (var db = new AlumnosContext())
+                {
+                    //Alumno al = db.Alumno.Where(a => al.Id == id).FirstOrDefault();
+                    Alumno al2 = db.Alumno.Find(id);
+                    return View(al2);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(Alumno a)
+        {
+            try
+            {
+                using (var db = new AlumnosContext())
+                {
+                    Alumno al = db.Alumno.Find(a.Id);
+                    al.Nombre = a.Nombre;
+                    al.Sexo = a.Sexo;
+                    al.Apellidos = a.Apellidos;
+                    al.Edad = a.Edad;
+                    db.SaveChanges();
+                    //return View();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+        public ActionResult Detalles(int id)
+        {
+            try
+            {
+                using (var db = new AlumnosContext())
+                {
+                    //Alumno al = db.Alumno.Where(a => al.Id == id).FirstOrDefault();
+                    Alumno al2 = db.Alumno.Find(id);
+                    return View(al2);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public ActionResult Eliminar(int id)
+        {
+            try
+            {
+                using (var db = new AlumnosContext())
+                {
+                    //Alumno al = db.Alumno.Where(a => al.Id == id).FirstOrDefault();
+                    Alumno al2 = db.Alumno.Find(id);
+                    db.Alumno.Remove(al2);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public ActionResult ListaCiudades()
+        {
+            using (var db = new AlumnosContext())
+            {
+                return PartialView(db.Ciudad.ToList());
+            }
+        }
+
+        public static string NombreCiudad(int CodCiudad)
+        {
+            using (var db = new AlumnosContext())
+            {
+                return db.Ciudad.Find(CodCiudad).Nombre;
+            }
+        }
+    }
+}
---
+++ CRUD-Alumnos/Models/AlumnoCE.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_Alumnos.Models
+{
+    public class AlumnoCE
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Ingrese Nombres")]
+        public string Nombre { get; set; }
+        [Required]
+        [Display(Name = "Ingrese Apellidos")]
+        public string Apellidos { get; set; }
+        [Required]
+        [Display(Name = "Ingresa Edad")]
+        public Nullable<int> Edad { get; set; }
+        [Required]
+        [Display(Name = "Ingrese Sexo")]
+        public string Sexo { get; set; }
+        [Required]
+        [Display(Name = "Ingrese Ciudad")]
+        public string CodCiudad { get; set; }
+        public string NombreCiudad { get; set; }
+        public string NombreCompleto { get { return Nombre + " " + Apellidos; } }
+        public System.DateTime FechaRegistro { get; set; }
+    }
+
+    [MetadataType(typeof(AlumnoCE))]
+    public partial class Alumno
+    {
+        public string NombreCompleto { get { return Nombre + " " + Apellidos; } }
+
+        public string NombreCiudad { get; set; }
+    }
+}
